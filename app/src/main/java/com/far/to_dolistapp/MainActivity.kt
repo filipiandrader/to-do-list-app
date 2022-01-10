1c0eb3517ae4867cb1c70a12e5e2844e340f916c
Filipi Andrade Rocha <filipiandrader@gmail.com>
2022-01-10 20:44:15 -0300

    Initial commit

---
+++ app/src/main/java/com/far/to_dolistapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.far.to_dolistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.far.to_dolistapp.ui.theme.ToDoListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToDoListAppTheme {
+        Greeting("Android")
+    }
+}
